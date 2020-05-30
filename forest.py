48ce44ea0be14f241dbb09e88b9305fed30025f7
A.Limonov <a-limonov@mail.ru>
2020-05-30 11:52:53 +0300

    Построение первого ряда перенесено в функцию

--- forest.py
+++ forest.py
@@ -1,38 +1,50 @@
 from tkinter import Tk, Canvas, Frame, BOTH
 import time
+import random
 
 
 class Object:
-    def __init__(self, x, y):
+    def __init__(self, x, y, colors):
         self.x = x
         self.y = y
+        self.colors = colors
 
 
 class Tree(Object):
-    def __init__(self, x, y):
-        Object.__init__(self, x, y)
+    def __init__(self, x, y, colors):
+        Object.__init__(self, x, y, colors)
+        canvas.update()
         canvas.create_rectangle(x, y, x + 20, y + 20, outline="#bb4508", fill="#bb4508")
-        canvas.create_polygon(x - 40, y, x + 10, y - 50, x + 60, y, x - 40, y, fill="green")
-        canvas.create_polygon(x - 30, y - 30, x + 10, y - 70, x + 50, y - 30, x - 30, y - 30, fill="green")
-        canvas.create_polygon(x - 20, y - 50, x + 10, y - 90, x + 40, y - 50, x - 20, y - 50, fill="green")
+        canvas.create_polygon(x - 40, y, x + 10, y - 50, x + 60, y, x - 40, y, fill=colors)
+        canvas.create_polygon(x - 30, y - 30, x + 10, y - 70, x + 50, y - 30, x - 30, y - 30, fill=colors)
+        canvas.create_polygon(x - 20, y - 50, x + 10, y - 90, x + 40, y - 50, x - 20, y - 50, fill=colors)
+        canvas.after(400)
         canvas.pack(fill=BOTH, expand=1)
 
 
-def main():
+def draw_first_yard():
+    """
+    Функция отвечает за отрисовку первого ряда ёлочек. По условиям первый ряд состояит из 10 ёлочек,
+    цвет - зеленый, x,y - координаты левого верхнего угла ствола
+    """
     x = 70
     y = 100
-    root = Tk()
-    global canvas
-    canvas = Canvas(root, width=1024, height=800, bg='white')
-    canvas.pack
-    i = 0
-    while i < 10:
-        draw_tree = Tree(x, y)
+    for i in range(10):
+        #colors = "red,orange,yellow,green,lightBlue,blue,violet,pink,grey,black,gold,silver".split(",")
+        colors = "green"
+        draw_tree = Tree(x, y, colors)
         x += 100
-        i += 1
-    root.geometry("1055x600")
-    root.title("Рисуем лес")
-    root.mainloop()
+
+
+def main():
+    global canvas
+    tk = Tk()
+    canvas = Canvas(tk, width=1055, height=600, bg='light green')
+    canvas.pack
+    draw_first_yard()
+    tk.geometry("1055x600")
+    tk.title("Рисуем лес")
+    tk.mainloop()
 
 
 if __name__ == '__main__':
